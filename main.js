f1316869de3f4603a99068e50925da5cd8a41b85
oriolpando <34099338+oriolpando@users.noreply.github.com>
2019-03-01 12:50:31 +0100

    c4

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 
 
 function getStats(){
-  console.log("fuck");
   
     $.ajax({ 
         type : "GET", 
         url : "https://public-api.tracker.gg/apex/v1/standard/profile/5/aleoriol", 
-        headers: {'Authorization': '1a8b1451-77c6-455d-84d9-88d1eb71ccf9', 'Content-type': 'application/x-www-form-urlencoded'},
-
+        headers: {'TRN-Api-Key': '1a8b1451-77c6-455d-84d9-88d1eb71ccf9'},
         success : function(result) { 
             console.log(result);
     
