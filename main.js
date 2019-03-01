bb7edfd551fd8fab936a3d935608c0e18c59f881
oriolpando <34099338+oriolpando@users.noreply.github.com>
2019-03-01 12:53:05 +0100

    crt

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function getStats(){
         type : "GET", 
         url : "https://public-api.tracker.gg/apex/v1/standard/profile/5/aleoriol", 
         headers: {'TRN-Api-Key': '1a8b1451-77c6-455d-84d9-88d1eb71ccf9'},
+        crossDomain: true,
         success : function(result) { 
             console.log(result);
     
