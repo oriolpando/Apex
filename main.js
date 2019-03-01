2ce549272d8194dd09bdef3b68e07004c38a3c4b
oriolpando <34099338+oriolpando@users.noreply.github.com>
2019-03-01 12:16:34 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ function getStats(){
     $.ajax({ 
         type : "GET", 
         url : "https://public-api.tracker.gg/apex/v1/standard/profile/5/aleoriol", 
-        headers: {'Authorization': '1a8b1451-77c6-455d-84d9-88d1eb71ccf9'},
+        headers: {'Authorization': '1a8b1451-77c6-455d-84d9-88d1eb71ccf9', 'Content-type': 'application/x-www-form-urlencoded'},
+
         success : function(result) { 
             console.log(result);
     
