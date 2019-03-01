d0fb24c118b308a4cfaeb2456e30ad94f85c67bc
oriolpando <34099338+oriolpando@users.noreply.github.com>
2019-03-01 12:02:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+function getStats(){
+    $.ajax({ 
+        type : "GET", 
+        url : "https://public-api.tracker.gg/apex/v1/standard/profile/5/aleoriol", 
+        headers: {'Authorization': '1a8b1451-77c6-455d-84d9-88d1eb71ccf9'},
+        success : function(result) { 
+            console.log(result);
+    
+        }, 
+        error : function(result) { 
+          //handle the error 
+        } 
+      }); 
+}
+
