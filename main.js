4d496fd9cd0ef9ef87ffdf0a960a101fe5ccd19d
oriolpando <34099338+oriolpando@users.noreply.github.com>
2019-03-06 20:29:27 +0100

    Try

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function getStats(){
     $.ajax({ 
         type : "GET", 
         url : "https://public-api.tracker.gg/apex/v1/standard/profile/5/aleoriol", 
-        headers: {'TRN-Api-Key': '1a8b1451-77c6-455d-84d9-88d1eb71ccf9'},
+        headers: {'TRN-Api-Key': '1a8b1451-77c6-455d-84d9-88d1eb71ccf9', 'Access-Control-Allow-Origin': '*'},
         crossDomain: true,
         dataType: 'application/json; charset=utf-8',
         success : function(result) { 
