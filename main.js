1a3207abf810f894733f1a31dd283ebcd55b5700
oriolpando <34099338+oriolpando@users.noreply.github.com>
2019-03-07 13:52:30 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
 
-
 function getStats(){
+
   
     $.ajax({ 
         type : "GET", 
-        url : "https://public-api.tracker.gg/apex/v1/standard/profile/5/aleoriol", 
-        headers: {'TRN-Api-Key': '1a8b1451-77c6-455d-84d9-88d1eb71ccf9', 'Access-Control-Allow-Origin': '*'},
-        crossDomain: true,
-        dataType: 'application/json; charset=utf-8',
+        url : "https://www.apexlegendshut.com/free-api?platform=PC&title=Aleoriol", 
+        dataType: "json",
         success : function(result) { 
-            console.log(result);
+          var player = JSON.parse(result);
+            console.log(player);
     
-        }, 
-        error : function(result) { 
-          console.log("fail")
-        } 
+        }
       });
 
-    window.open("/Apex/statsPage.html?param1=aleoriol")
+    //window.location.href = "statsPage.html?param1=aleoriol";
+    
 }
 
